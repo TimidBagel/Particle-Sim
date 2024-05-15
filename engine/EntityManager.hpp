a7d9e944ea094bbedc455f7ec2fc39762e705c52
Iain Broomell <slaprabbit805@gmail.com>
2024-05-15 13:48:55 -0400

    Merged into components, updated "update" function invalid naming convention

--- engine/EntityManager.hpp
+++ engine/EntityManager.hpp
@@ -4,9 +4,22 @@
 #include "Entity.hpp"
 
 static class EntityManager {
+private:
 	std::vector<std::shared_ptr<Entity>> entities;
 
+public:
 	void add_entity(std::shared_ptr<Entity> entity) {
 		entities.push_back(entity);
+	}
+
+	int remove_entity(std::shared_ptr<Entity> doomed_entity) {
+		int entity_index = 0;
+		for (auto& iter_entity : entities) {
+			if (iter_entity == doomed_entity) {
+				entities.erase(entities.begin() + entity_index);
+				return entity_index;
+			}
+			entity_index++;
+		}
 	}
 };
