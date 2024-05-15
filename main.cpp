73741f8f2f994114bce752ea29f2ef956fdd1dba
Ben Champagne <donkeykong797@gmail.com>
2024-05-15 13:25:17 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,36 @@
 #include<iostream>
-using namespace std;
 
 #include "include/SFML/Graphics.hpp"
 
-static void game_logic(sf::Time deltaTime) { return; }
+static void simulate(sf::Time delta) {
+    std::cout << delta.asMicroseconds();
+}
 
 int main() {
-	return 0;
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::Clock clock;
+    sf::Time time_curr;
+    sf::Time time_prev;
+
+    while (window.isOpen())
+    {
+        time_curr = clock.getElapsedTime();
+        sf::Time delta = time_curr - time_prev;
+        time_prev = time_curr;
+
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        simulate(delta);
+        window.display();
+
+        clock.restart();
+    }
+
+    return 0;
 }
