29cd566c69095130618184a006c02a2c84e61ff6
Iain Broomell <slaprabbit805@gmail.com>
2024-05-15 11:20:56 -0400

    initialized project files for the final time oml

--- main.cpp
+++ main.cpp
@@ -2,14 +2,9 @@
 using namespace std;
 
 #include "SFML/Graphics.hpp"
-#include "Particle.hpp"
-#include "Entity.hpp"
-#include "Component.hpp"
 
 static void gameLogic(sf::Time deltaTime) { return; }
 
 int main() {
-	Entity player;
-	Component health;
 	return 0;
 }
