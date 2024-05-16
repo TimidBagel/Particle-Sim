3ee5d497dd442204b92345706c55c8427b2d8313
KitChaos <cmeow176@gmail.com>
2024-05-16 13:26:12 -0400

    Removed extra

--- build.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/bash
-g++ -c $1.cpp 
-g++ $1.o -o $1-app -lsfml-graphics -lsfml-window -lsfml-system
--- commit.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/bash
-read -p "Enter Commit Message: " comMes
-git add .
-git commit -m "${comMes}"
-git push -u origin pixelRender
